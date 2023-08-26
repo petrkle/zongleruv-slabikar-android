04b2991fb695c1dc7b3fdfb2d7be9634983e4e52
Petr <petr@kle.cz>
2023-08-26 22:17:47 +0200

    Formátování

--- app/src/main/java/info/zonglovani/mobile/app/Zonglovani.java
+++ app/src/main/java/info/zonglovani/mobile/app/Zonglovani.java
@@ -3,13 +3,13 @@ package info.zonglovani.mobile.app;
 import android.os.Bundle;
 import org.apache.cordova.*;
 
-public class Zonglovani extends CordovaActivity 
+public class Zonglovani extends CordovaActivity
 {
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-				Bundle extras = getIntent().getExtras();
+        Bundle extras = getIntent().getExtras();
         if (extras != null && extras.getBoolean("cdvStartInBackground", false)) {
             moveTaskToBack(true);
         }
