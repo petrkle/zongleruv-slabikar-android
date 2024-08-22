815a392715516c98d5a0a50cee22e7dc2b6cbaf5
Petr <petr@kle.cz>
2024-08-22 20:34:47 +0200

    Formátování

--- uprav.php
+++ uprav.php
@@ -1,21 +1,21 @@
 <?php
 
-$wget=[];
+$wget = [];
 
 $log = file_get_contents('log.txt');
 $log = preg_replace('/FINISHED.*/s', '', $log);
 $log = preg_split('/ saved /', $log);
 
-foreach($log as $foo){
-  $foo = trim($foo);
-  if(strlen($foo)>0){
-    $foo = preg_replace('/\s+/', ' ', $foo);
-    $url = preg_replace('/.*http:\/\/localhost:4567([^ ]*) .*/', '$1', $foo);
-    $file = preg_replace("#.*app/src/main/assets/www/([^']*)'.*#", '$1', $foo);
-    if(preg_match('/^\//', $url)){
-      $wget[$url] = $file;
+foreach($log as $foo) {
+    $foo = trim($foo);
+    if(strlen($foo) > 0) {
+        $foo = preg_replace('/\s+/', ' ', $foo);
+        $url = preg_replace('/.*http:\/\/localhost:4567([^ ]*) .*/', '$1', $foo);
+        $file = preg_replace("#.*app/src/main/assets/www/([^']*)'.*#", '$1', $foo);
+        if(preg_match('/^\//', $url)) {
+            $wget[$url] = $file;
+        }
     }
-  }
 }
 
 $jsfile = glob('app/src/main/assets/www/*.js')[0];
@@ -26,14 +26,14 @@ $js = preg_replace('#/img/s/#', '', $js);
 
 $nahrady = file($jsfile, FILE_IGNORE_NEW_LINES);
 
-foreach($nahrady as $line){
-  if(preg_match('/d\.push/', $line)){
-    $url = preg_replace('/.*p:"/', '', $line);
-    $url = preg_replace('/".*/', '', $url);
-    if(isset($wget[$url])){
-      $js = preg_replace('#"'.$url.'"#', '"'.$wget[$url].'"', $js);
+foreach($nahrady as $line) {
+    if(preg_match('/d\.push/', $line)) {
+        $url = preg_replace('/.*p:"/', '', $line);
+        $url = preg_replace('/".*/', '', $url);
+        if(isset($wget[$url])) {
+            $js = preg_replace('#"'.$url.'"#', '"'.$wget[$url].'"', $js);
+        }
     }
-  }
 }
 
 file_put_contents($jsfile, $js);
